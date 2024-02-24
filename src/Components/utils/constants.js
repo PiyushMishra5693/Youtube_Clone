290e6e5df0c98b47f5f0ab68386a6c92eed25da2
PiyushMishra5693 <pm455567@gmail.com>
2024-02-24 16:25:13 +0530

    tenth commit

--- src/Components/utils/constants.js
+++ src/Components/utils/constants.js
@@ -1,5 +1,5 @@
 
-const YOUTUBE_VIDEO_LIST ='https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=48&regionCode=IN&key=AIzaSyArahdqMGSi7x4F-jfVd9Pjoy7KVuiw4ks';
+const YOUTUBE_VIDEO_LIST ='https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=48&regionCode=IN&key=AIzaSyBZbwcI2sGG66XVkgnmRuu3nkG_LQ3Rqbc';
 
 export const YOUTUBE_SEARCH_RESULT= 'https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q='
 
