010176592a2f26819f70f453c7e5d7a655e314b9
PiyushMishra5693 <pm455567@gmail.com>
2024-03-01 14:55:23 +0530

    desc and format view

--- src/Components/Videolist.jsx
+++ src/Components/Videolist.jsx
@@ -35,8 +35,8 @@ return (
             
             <img  className=" rounded-xl "src ={img_url} alt="Video Thumbnail" />
             <h1 className={'mt-2 font-bold ml-2 py-2 '+(IsDark?"text-white":"text-black")}>{channel_desc}</h1>
-            <h1 className='ml-2 text-[16px] text-[#AAAAAA]'>{channel_name}</h1>
-            <h1 className='ml-2 text-[16px] text-[#AAAAAA]'>{formattedViews} Views</h1>
+            <h1 className={'ml-2 text-[16px] '+(IsDark?"text-[#AAAAAA]":"text-black")}>{channel_name}</h1>
+            <h1 className={'ml-2 text-[16px] '+(IsDark?"text-[#AAAAAA]":"text-black")}>{formattedViews} Views</h1>
     </div>
   )
 }
